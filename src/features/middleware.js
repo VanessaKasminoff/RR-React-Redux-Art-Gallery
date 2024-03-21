92f330738ea5500a0c0596856f26bdb6ac78b1d2
Vanessa Kasminoff <sunnset07@gmail.com>
2024-03-20 21:40:42 -0700

    finished assignment

--- src/features/middleware.js
+++ src/features/middleware.js
@@ -1,6 +1,8 @@
 export const logger = store => next => action => {
+
     if (typeof action === 'function') {
         action(store.dispatch, store.getState)
+        
     } else { 
         console.log('dispatch', store.getState())
         next(action)
